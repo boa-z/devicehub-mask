c8cad79a31364e53bb8c20b2d6258ebb65ceadc2
boa-z <boa-z@outlook.com>
2026-07-25 00:11:35 +0800

    Fix keyboard icon sizing across platforms

--- src/components/KeyboardIcon.tsx
+++ src/components/KeyboardIcon.tsx
@@ -1,3 +1,9 @@
 export function KeyboardIcon() {
-  return <span className="anticon keyboard-icon" aria-hidden="true">{"\u2328\uFE0E"}</span>;
+  return (
+    <span className="anticon keyboard-icon" aria-hidden="true">
+      <svg viewBox="0 0 24 24" focusable="false">
+        <path d="M4 5h16a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V7a2 2 0 0 1 2-2Zm0 2v10h16V7H4Zm1 1h2v2H5V8Zm3 0h2v2H8V8Zm3 0h2v2h-2V8Zm3 0h2v2h-2V8Zm3 0h2v2h-2V8ZM5 11h2v2H5v-2Zm3 0h2v2H8v-2Zm3 0h2v2h-2v-2Zm3 0h2v2h-2v-2Zm3 0h2v2h-2v-2ZM5 14h14v2H5v-2Z" />
+      </svg>
+    </span>
+  );
 }
